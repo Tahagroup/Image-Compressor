fb125c66d8e167847c94ad8d67eb8cfa71b70e4d
Archer <y.basiri@yahoo.com>
2022-11-12 16:07:50 +0330

    project re-structure

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import ThemeContexProvider from "./context/ThemeContext";
+import ThemeContexProvider from "./components/context/ThemeContext";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
